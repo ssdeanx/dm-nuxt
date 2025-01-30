e7d4551ae0824cbc4c1ddd43d8afb12da0245743
ssdeanx <ssdeanx@gmail.com>
2025-01-30 05:46:34 -0500

    add Nuxt types and configuration updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/color-mode',
-        '@nuxt/image',
-    ],
-    css:[
-        '~/assets/css/app.css'
-    ],
-    colorMode:{
-        preference: 'system', // default value of $colorMode.preference
-        fallback: 'dark', // fallback value if not system preference found
-        hid: 'nuxt-color-mode-script',
-        globalName: '__NUXT_COLOR_MODE__',
-        componentName: 'ColorScheme',
-        classPrefix: '',
-        classSuffix: '',
-        storageKey: 'nuxt-color-mode'
+  modules: ['@nuxtjs/color-mode', '@nuxt/image', '@vueuse/nuxt', '@nuxt/icon'],
+
+  css:[
+      '~/assets/css/app.css'
+  ],
+
+  colorMode:{
+      preference: 'system', // default value of $colorMode.preference
+      fallback: 'dark', // fallback value if not system preference found
+      hid: 'nuxt-color-mode-script',
+      globalName: '__NUXT_COLOR_MODE__',
+      componentName: 'ColorScheme',
+      classPrefix: '',
+      classSuffix: '',
+      storageKey: 'nuxt-color-mode'
+  },
+
+  postcss:{
+      plugins: {
+          tailwindcss: {},
+          autoprefixer: {}
+      }
+  },
+
+  devtools: {
+    timeline: {
+      enabled: true,
     },
-    postcss:{
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {}
-        }
-    },
+  },
+
+  compatibilityDate: '2025-01-30',
 })
